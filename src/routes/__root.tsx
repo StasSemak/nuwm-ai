e7b9f5203706641079de66fb7f40a6c01c929156
StasSemak <semaks051@gmail.com>
2024-02-02 19:09:54 +0200

    feat: setup tanstack router

---
+++ src/routes/__root.tsx
@@ -0,0 +1,21 @@
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+
+export const Route = createRootRoute({
+    component: RootLayout,
+})
+
+function RootLayout() {
+    return(
+        <>
+            <nav className='flex gap-3'>
+                <Link to="/">
+                    Home
+                </Link>
+                <Link to="/sign-in">
+                    Sign-in
+                </Link>
+            </nav>
+            <Outlet/>
+        </>
+    )
+}
---
+++ src/routes/sign-in.lazy.tsx
@@ -0,0 +1,13 @@
+import { createLazyFileRoute } from '@tanstack/react-router';
+
+export const Route = createLazyFileRoute('/sign-in')({
+    component: About,
+})
+
+function About() {
+  return(   
+    <div className="p-2">
+        <h3>Sign-in page</h3>
+    </div>
+  )
+}
