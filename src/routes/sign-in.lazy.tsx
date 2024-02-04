018c66b61fe15e57ccfbfc42a67b6f72fb716b8e
StasSemak <semaks051@gmail.com>
2024-02-04 13:27:19 +0200

    feat: sign-in page layout

--- src/routes/sign-in.lazy.tsx
+++ src/routes/sign-in.lazy.tsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { Button } from "../components/ui/button";
 
 export const Route = createLazyFileRoute("/sign-in")({
   component: SignIn,
@@ -6,8 +7,11 @@ export const Route = createLazyFileRoute("/sign-in")({
 
 function SignIn() {
   return (
-    <div className="p-2">
-      <h3>Sign-in page</h3>
+    <div className="h-full flex flex-col items-center">
+      <div className="max-w-[600px] w-full bg-secondary mt-32 flex flex-col gap-5 py-6 px-8 rounded-md items-center">
+        <h2 className="text-zinc-100 text-2xl">Авторизація</h2>
+        <Button variant="light" className="w-full">Увійти</Button>
+      </div>
     </div>
   );
 }
