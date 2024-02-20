ab763262a21062263be068a90222a3596bb148c6
StasSemak <semaks051@gmail.com>
2024-02-20 11:56:23 +0200

    chore: delete unused files, run prettier

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import "./styles/embed.css";
-import { Index } from "./routes";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Index/>
-);
