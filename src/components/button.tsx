d2c5b6c7be53209bf245a1ad1ad7dcd68ac3b2b6
StasSemak <semaks051@gmail.com>
2024-02-20 12:40:10 +0200

    feat: optimize tailwind config

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,7 @@ const Button = React.forwardRef<HTMLButtonElement, Props>(
     return (
       <button
         className={cn(
-          "inline-flex items-center justify-center rounded-md text-base font-medium transition-all disabled:opacity-50 disabled:pointer-events-none bg-main hover:bg-main-80 text-zinc-100 h-10 py-2 px-4",
+          "inline-flex items-center justify-center rounded-md text-base font-medium transition-all disabled:opacity-50 disabled:pointer-events-none bg-main hover:bg-main/80 text-zinc-100 h-10 py-2 px-4",
           className
         )}
         ref={ref}
