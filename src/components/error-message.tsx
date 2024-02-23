aeec6be96f074e28db188e98c1e36aa8efe9fb59
StasSemak <semaks051@gmail.com>
2024-02-23 18:11:33 +0200

    feat: status elements margins

--- src/components/error-message.tsx
+++ src/components/error-message.tsx
@@ -2,7 +2,7 @@ import { AlertCircle } from "lucide-react";
 
 export function ErrorMessage() {
   return (
-    <div className="flex-grow flex flex-col justify-center items-center">
+    <div className="flex-grow flex flex-col justify-center items-center mt-5">
       <div className="flex gap-2 items-center">
         <AlertCircle className="size-8 stroke-main" />
         <p className="text-main text-xl">Виникла помилка</p>
--- src/components/loading-spinner.tsx
+++ src/components/loading-spinner.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export function LoadingSpinner() {
   return (
-    <div className="flex-grow flex flex-col items-center justify-center">
+    <div className="flex-grow flex flex-col items-center justify-center mt-5">
       <Loader2 className="stroke-main size-12 animate-spin" />
     </div>
   );
