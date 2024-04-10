f4e6eed19badf9d2590b250a98cd715ec7310aa1
StasSemak <semaks051@gmail.com>
2024-04-10 17:48:17 +0300

    feat: app as spa

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,20 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Index } from "./routes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Admin } from "./routes/admin";
+import { History } from "./routes/history";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <Index />
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Index />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </BrowserRouter>
   </QueryClientProvider>
 );
