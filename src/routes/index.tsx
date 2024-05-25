f64a07e7edfa04e92da19295545733f05223eca2
StasSemak <semaks051@gmail.com>
2024-05-25 21:47:36 +0300

    feat: footer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
+
 export function Index() {
   return (
-    <div className="w-full max-w-[1240px] my-0 mx-auto pt-8 px-3 sm:px-6 md:px-12">
+    <div className="w-full max-w-[1240px] my-0 mx-auto pt-8 px-3 sm:px-6 md:px-12 flex flex-col gap-5">
       <div className="p-2 flex flex-col items-center bg-zinc-100 rounded-md h-[85vh]">
         <iframe
           src="/embed.html"
@@ -10,6 +12,22 @@ export function Index() {
           style={{ border: "0px", flexGrow: "1" }}
         /> 
       </div>
+      <Footer/>
     </div>
   );
+}
+
+function Footer() {
+  return(
+    <div className="flex flex-col gap-3 w-full text-zinc-700 text-sm">
+      <p>Чат-бот Національного університету водного господарства та природокористування на основі генеративного штучного інтелекту. Надає відповіді на запитання щодо вступної кампанії.</p>
+      <div className="flex gap-12 items-center">
+        <a className="" href="https://nuwm.edu.ua">Веб-сайт НУВГП</a>
+        <a href="https://t.me/NUWEEAI_bot">Чат-бот у Telegram</a>
+        <Link to="/admin">
+          Для адміністратора
+        </Link>
+      </div>
+    </div>
+  )
 }
