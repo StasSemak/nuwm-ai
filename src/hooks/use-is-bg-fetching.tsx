ad1d2397c28e6dd551a166c0083bb506599e8ca1
StasSemak <semaks051@gmail.com>
2024-06-02 13:11:31 +0300

    feat: updated hook with optimizied solution

--- src/hooks/use-is-bg-fetching.tsx
+++ src/hooks/use-is-bg-fetching.tsx
@@ -1,20 +1,11 @@
-import { QueryClient, QueryFilters, notifyManager, useQueryClient } from "@tanstack/react-query";
-import React from "react";
+import { QueryClient, QueryFilters, useIsFetching } from "@tanstack/react-query";
 
 export function useIsBgFetching(
   filters?: QueryFilters,
   queryClient?: QueryClient,
 ): number {
-  const client = useQueryClient(queryClient)
-  const queryCache = client.getQueryCache()
-
-  return React.useSyncExternalStore(
-    React.useCallback(
-      (onStoreChange) =>
-        queryCache.subscribe(notifyManager.batchCalls(onStoreChange)),
-      [queryCache],
-    ),
-    () => client.getQueryState(filters?.queryKey ?? [""])?.status !== "pending" ? client.isFetching(filters) : 0,
-    () => client.getQueryState(filters?.queryKey ?? [""])?.status !== "pending" ? client.isFetching(filters) : 0,
-  )
+  return useIsFetching({
+    predicate: (query) => query.state.data !== undefined,
+    ...filters,
+  }, queryClient);
 }
